3cac02ea62f7cc956482efce0c8ad9fba1915745
Sergei Romanov <4romanov4@gmail.com>
2023-04-12 19:00:56 +0200

    Add text description

--- src/constants/aboutMe.ts
+++ src/constants/aboutMe.ts
@@ -1,8 +1,8 @@
 import { EAboutMeTypes } from './enum/ECommonTypes';
 
 const mapAboutMeTypesToValue = {
-    [EAboutMeTypes.career]: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu consectetur nulla. Pellentesque malesuada nulla ut justo ullamcorper, id posuere massa rhoncus. Pellentesque nunc dolor, eleifend vitae ultricies ullamcorper, eleifend suscipit augue. Aenean ipsum enim, vehicula vitae mattis ut, sagittis non massa. Etiam pretium porta suscipit.',
-    [EAboutMeTypes.life]: 'text about life'
+    [EAboutMeTypes.career]: 'I`m a frontend developer with 4 years of experience. My primary technology stack includes React, Redux, and TypeScript. My current position is frontend leader team. I have 3 developers under me.\n\nI am committed to writing clean code and understand the importance of a user-friendly and intuitive interface. I enjoy creating responsive and accessible web applications that meet the needs of users and make their lives easier. I`m passionate about learning and strive to improve my development skills every day. I would be happy to collaborate with you in the future!',
+    [EAboutMeTypes.life]: 'I have been living in Budapest last 6 month. I live here as Digital Nomad. I really like it here and I want to stay and live here.\n\nI graduated from the Mordovian State University with B.A. in Electronic Engineering.\n\nIn my free time, I love exploring new places in the city, play volleyball and trying something new. And I also love animals'
 };
 
 export default mapAboutMeTypesToValue;
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,37 +3,42 @@ import { IProjectsItem } from 'interfaces/common';
 const PROJECTS: IProjectsItem[] = [
     {
         name: 'New Digital Economy League website',
-        description: 'New main website of my organisation',
+        description: 'The project of a new website of my organization using modern technologies and design. In the future, it will replace the current site and also give the recruiter the opportunity to create an offer for new employees\n\nThis is an important project for me since the main site is the face of the organization (6 000 employees).\n\nI am responsible for the entire frontend part on this project. From setup to deployment',
         link: 'https://projects.digitalleague.ru/'
     },
     {
         name: 'Digital Business Card',
-        description: 'Individual employee`s cards to share personal informantion and contacts for stregthen the work community and improve the level of inclusion',
+        description: 'Employee`s digital business card. it was conceived for internal use for various internal events. But now this project has gone beyond the internal and it is possible to create a business card for anyone\n\nThis is my first project that I did myself, as a front end development leader from the scratch to the release of the and further support',
         link: 'https://cards.digitalleague.ru/946b070f-ba61-496a-900d-0afeb3e03e9a'
     },
     {
         name: 'RusAg Svoe selo',
-        description: 'Development of a website for one of the major Russian banks, which enables clients to use digital bank products',
+        description: 'A project for the top-5 of the Bank of Russia. Allows you to find a plot of land or a house project, apply online for a mortgage loan\n\nWork in a large team (8 frontend developers). Fintech infrastructure has allowed a deep understanding of business processes.',
         link: 'https://svoe-selo.ru/'
     },
     {
         name: 'RusAg Svoe dom',
-        description: 'Development of a website for one of the major Russian banks, which enables clients to use digital bank products',
+        description: 'A project for the top-5 of the Bank of Russia. Allows you to find an apartment and choose a program, then submit an online application for a mortgage loan\n\nWork in a large team (8 frontend developers). Fintech infrastructure has allowed a deep understanding of business processes.',
         link: 'https://svoedom.ru/'
     },
     {
         name: 'RusAg digital products',
-        description: 'Development of a website for one of the major Russian banks, which enables clients to use digital bank products',
+        description: 'A project for the top-5 of the Bank of Russia. Allows you to find various credit programs, choose the type of bank card, pick up a safe deposit box and other digital products of the bank, and then submit an online application for the selected product\n\nWork in a large team (8 frontend developers). Fintech infrastructure has allowed a deep understanding of business processes.',
         link: 'https://retail.rshb.ru/'
     },
     {
+        name: 'Tesler Platform',
+        description: 'Tesler UI is an open source library that supplies user interaction support for Tesler framework in form of React components, Redux reducers and redux-observable epics for handling asynchronous actions.',
+        link: 'https://github.com/tesler-platform/tesler-ui'
+    },
+    {
         name: 'Ekilu',
-        description: 'Recipe search app. Searching depends on your ingredients',
+        description: 'An application for finding a recipe by available ingredients.\n\nI worked as a full stack developer, and also did a webview for a cross-platform application',
         link: 'https://ekilu.com/es'
     },
     {
-        name: 'Other',
-        description: 'There are another projects witch I can`t show you, becouse this is inner projects for different companies'
+        name: 'Other projects',
+        description: 'Other projects that I can`t show, as they are for internal use by various organizations, mainly in the banking sector'
     }
 ];
 
